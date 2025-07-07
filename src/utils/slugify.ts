25e71942c5b6afac8ffc685f30dfaf79927c65aa
trann <trannhukhoinguyen@gmail.com>
2025-07-07 11:09:48 +0700

    update seo

---
+++ src/utils/slugify.ts
@@ -0,0 +1,16 @@
+export function slugify(text: string): string {
+  return text
+      .normalize('NFD')
+      .replace(/[\u0300-\u036f]/g, '')
+      .toLowerCase()
+      .trim()
+      .replace(/[^a-z0-9\s-]/g, '')
+      .replace(/\s+/g, '-')
+}
+export function stringify(text: string): string {
+  return text
+      .normalize('NFD')
+      .replace(/[\u0300-\u036f]/g, '')
+      .toLowerCase()
+      .replace(/[^a-z0-9\s-]/g, '')
+}
