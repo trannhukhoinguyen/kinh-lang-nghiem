f4f6934acafaf5e0d422f2a0064d3b653b475cef
trann <trannhukhoinguyen@gmail.com>
2025-07-07 11:21:36 +0700

    update output: 'static',

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,20 +2,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
-import { SEO } from 'astro-seo'
 import robotsTxt from 'astro-robots-txt'
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'static',
   site: 'https://kinh-lang-nghiem.vercel.app', // Replace with your site URL
   integrations: [
     tailwind(),
     sitemap(),
-    SEO({
-      // Tuỳ chỉnh nếu muốn, mặc định đã đủ dùng
-      site: 'https://kinh-lang-gia.vercel.app', // <-- Thay bằng domain thật của bạn
-    }),
     robotsTxt({
       policy: [
         {
